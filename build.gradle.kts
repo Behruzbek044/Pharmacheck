f74eacf280475ae7c77467792c87f34da8dc2fb2
Akbarali <aorazimbetov03@gmail.com>
2024-05-23 11:21:00 +0500

    added libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -48,8 +51,23 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment:2.7.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+<<<<<<< HEAD
 >>>>>>> fa43152 (initial commit)
+=======
+
+    //Barcode scanner library
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
+    implementation("com.google.zxing:core:3.4.1")
+
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //LottieFiles library
+    implementation("com.airbnb.android:lottie:3.7.0")
+>>>>>>> 2c8bcf5 (added libraries)
 }
