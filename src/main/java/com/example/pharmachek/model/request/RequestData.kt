ff7c5c528f466ae21412ee5d3d718a6ca89664a3
Behruzbek044 <115205015+Behruzbek044@users.noreply.github.com>
2024-07-28 06:21:14 +0500

    Final changes

--- src/main/java/com/example/pharmachek/model/request/RequestData.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pharmachek.model.request
-
-data class RequestData(
-    val opr: String,
-    val det: String
-)
--- src/main/java/com/example/pharmachek/utils/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.example.pharmachek.utils
-
-object Constants {
-    const val DATABASE_NAME = "akbarali_final.db"
-    const val DEFAULT_FRAGMENT_NUMBER = 1
-    const val BASE_URL = "http://54.160.153.22:8082/"
-}
